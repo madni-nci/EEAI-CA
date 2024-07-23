7afc7fb66893c3e545d73dbd644a1322398dd6b0
Muhammad Hassan Shakeel <mhassan20896@gmail.com>
2024-07-24 00:58:16 +0100

    Implement Chained multi output modeling

--- model/randomforest.py
+++ model/randomforest.py
@@ -24,57 +24,86 @@ class RandomForest(BaseModel):
         super(RandomForest, self).__init__()
         self.model_name = model_name
         self.data = data
+        # initialize classifier for each target variable
         self.mdl = [RandomForestClassifier(n_estimators=1000, random_state=seed, class_weight='balanced_subsample') for _ in Config.CLASS_COLS]
         self.predictions = None
         self.data_transform()
 
     def train(self, data) -> None:
         print('training started')
-        self.mdl =[self.mdl[i].fit(data.X_train, data.y_train[Config.CLASS_COLS[i]]) for i in range(len(Config.CLASS_COLS))]
+        # train the classifier for each target variable
+        for i in range(len(Config.CLASS_COLS)):
+            if i > 0:
+               # add actual target (y_train) variable as a feature
+               data.X_train =  np.column_stack((data.X_train,data.y_train[Config.CLASS_COLS[i-1]].to_numpy()))
+            
+            # fit the i_th model
+            self.mdl[i].fit(data.X_train, data.y_train[Config.CLASS_COLS[i]])
         print('training finished')
 
     def predict(self, X_test: pd.Series):
         print('prediction started')
-        predictions = {Config.CLASS_COLS[i]: self.mdl[i].predict(X_test) for i in range(len(Config.CLASS_COLS))}
-        self.predictions = pd.DataFrame(predictions)
+
+        # predict the classifier for each target variables
+        for i in range(len(Config.CLASS_COLS)):
+            if i > 0:
+                # add prediction of previous model as a feature
+                X_test =  np.hstack((X_test, self.predictions[:,i-1].reshape(-1,1)))
+            predictions = self.mdl[i].predict(X_test)
+
+            # Initially self.predictions is None, so we need to initialize it
+            if i == 0:
+                self.predictions = predictions.reshape(-1,1)
+            else:
+                # append new predictions to the existing predictions
+                self.predictions = np.column_stack((self.predictions,predictions))
+        
+        # Convert the predictions to a pandas dataframe for easy manipulation
+        self.predictions = pd.DataFrame(self.predictions)
         print('prediction ended')
     
     def calculateAccuracy(self, row: pd.Series) -> double:
         correct_pred = 0
+
+        # calculate the accuracy for each row
         for value in row:
             if value == True:
                 correct_pred += 1
             else:
+                # no need to check further if the prediction is wrong
                 break
+
+        # len(Config.CLASS_COLS) is the total number of target variables, currently equals to 3
         return correct_pred / len(Config.CLASS_COLS) * 100
 
     def print_results(self, data):
         results = {f'Type {i+2}':[] for i in range(len(Config.CLASS_COLS))}
 
+        # compare prediction and actual y_test, then store the boolean in results variable
         for i in range(len(Config.CLASS_COLS)):
             results[f'Type {i+2}'] = self.predictions.iloc[:,i].to_numpy() == data.y_test[Config.CLASS_COLS[i]].to_numpy()
         
+        # Convert the results to a pandas dataframe for easy manipulation
         results_df = pd.DataFrame(results)
 
+        # Calculate the accuracy for each row
         results_df['Accuracy'] = pd.DataFrame({'accuracy': results_df.apply(self.calculateAccuracy, axis=1)})
+
+        # Calculate the total accuracy for all rows
         total_accuracy = results_df["Accuracy"].mean()
 
+        # print the results for each row
         for i in range(data.test_df.shape[0]):
             print(f'Content: {data.test_df.iloc[i,5]}')
             print(f'Predcited Classes:{list(self.predictions.iloc[i,:].to_numpy())}')
             print(f'True Classes:{list(self.data.y_test.iloc[i,:].to_numpy())}')
-            print(f'Accuracy: {self.calculateAccuracy(results_df.iloc[i,:])}%')
+            print(f'Accuracy: {self.calculateAccuracy(results_df.iloc[i,:]):.2f}%')
             print('\n')
-        
-        print('\n')
 
         return total_accuracy
 
-        # print(f'Total Accuracy: {results_df['Accuracy'].mean()}')
-        # print(classification_report(data.y_test, self.predictions))
-
-
     def data_transform(self) -> None:
+        # Transform the target variables to numerical values
         from sklearn.preprocessing import LabelEncoder
         le = LabelEncoder()
         self.data.y_train = pd.DataFrame(self.data.get_type_y_train())
@@ -86,3 +115,4 @@ class RandomForest(BaseModel):
             le.fit(self.data.y_train[column])
             self.data.y_train[column] = le.transform(self.data.y_train[column])
             self.data.y_test[column] = le.transform(self.data.y_test[column])
+
